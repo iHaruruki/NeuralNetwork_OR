266887f6e41d24a13da844fa14acc0954e097e77
Haruki Isono <haruki.isono861@gmail.com>
2025-01-17 00:10:42 +0900

    add: 2-input perceptron fanction

--- neuralnetwork_or.py
+++ neuralnetwork_or.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+# 2-input perceptron fanction
+def perceptron(x1,x2, w1, w2, theta):
+    f = x1*w1 + x2*w2
+    if f >= theta:
+        y = 0
+    else:
+        y = 1
+    return y
+
+x1 = np.array([0,0,1,1])
+x2 = np.array([0,1,0,1])
+w1 = 0.5
+w2 = 0.5
+theta = 0.2
+
+for i in range(4):
+    y = perceptron(x1[i], x2[i], w1, w2, theta)
+    print("x1 = ", x1[i], " x2 = ", x2[i], " y = ", y)
